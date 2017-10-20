eb0323eb9a106a71927698eb28a9888b67e70d35
xhlim <xhlim@outlook.com>
2017-10-20 14:36:23 +0800

    angular js

--- learn-js/learn-js-angularjs/src/main/java/com/xhlim/SpringBootJsApplication.java
+++ learn-js/learn-js-angularjs/src/main/java/com/xhlim/SpringBootJsApplication.java
@@ -2,21 +2,13 @@ package com.xhlim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author xhlim@outlook.com
  * @create 2017-09-08 14:30
  */
 @SpringBootApplication
-public class SpringBootJsApplication extends SpringBootServletInitializer {
-
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(SpringBootJsApplication.class);
-    }
+public class SpringBootJsApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJsApplication.class, args);
